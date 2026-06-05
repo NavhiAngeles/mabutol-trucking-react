690f51ceca81c6efb747d2352ae57d6f2a8b9807
NavhiAngeles <ivhanangeles.04@gmail.com>
2026-06-05 16:19:04 +0800

    done ui cleaning

--- src/pages/Fleet/fleet.jsx
+++ src/pages/Fleet/fleet.jsx
@@ -1,4 +1,4 @@
-// fleet.jsx
+﻿// fleet.jsx
 import { useState } from "react";
 import MainLayout from "../../layouts/mainLayout";
 import "./fleet.css";
@@ -156,7 +156,7 @@ const handleCargoToggle = (id) => {
           </div>
           <div className="tab-pill-box">
           <button
-            className={`tab-pill ${vehicleTab === "compliance" ? "active" : ""}`}
+            className={`tab-pill ${vehicleTab === "details" ? "active" : ""}`}
             onClick={() => setVehicleTab("details")}
             type="button"
             > <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{marginRight: '4px'}}><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>
